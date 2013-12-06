103ae8397778e094c5c41317ef168edbe063e462
Isaac Lopes <contato@isaaclopes.com.br>
2013-12-06 21:58:39 -0200

    atualiza

--- app/code/local/Inovarti/Templatedesign/Model/Config/Zoom/Position.php
+++ app/code/local/Inovarti/Templatedesign/Model/Config/Zoom/Position.php
@@ -13,8 +13,17 @@ class Inovarti_Templatedesign_Model_Config_Zoom_Position
     {
         return array(
             array(
+	            'value'=>'top',
+	            'label' => Mage::helper('templatedesign')->__('Top')),
+            array(
 	            'value'=>'right',
 	            'label' => Mage::helper('templatedesign')->__('Right')),
+            array(
+	            'value'=>'bottom',
+	            'label' => Mage::helper('templatedesign')->__('Bottom')),
+            array(
+	            'value'=>'left',
+	            'label' => Mage::helper('templatedesign')->__('Left')),
             array(
 	            'value'=>'inside',
 	            'label' => Mage::helper('templatedesign')->__('Inside')),
